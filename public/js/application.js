10e8559faf6dfc53e72621253f67d1ce7d3f5368
Evangeline Garreau <ergarreau@gmail.com>
2015-07-24 14:54:09 -0500

    finish spread view

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 
+  $("div.card").on("click", function(){
+    $("div.card").removeClass("neg");
+    $("div.card").removeClass("pos");
+    $("div.card").removeClass("current");
+
+    cardId = $(this).attr("id");
+    negativeId = ".negative-"+cardId;
+    positiveId = ".positive-"+cardId;
+    $(this).addClass("current")
+    $(negativeId).addClass("neg");
+    $(positiveId).addClass("pos");
+  });
 
 });
